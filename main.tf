14a5fc2223384d09ad2aa2633c933952d112ca91
rveerasu <vrk.hcl@gmail.com>
2023-06-10 06:22:24 +0000

    infra

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">=1.3.0"
+  required_providers {
+    azurerm = {
+      "source" = "hashicorp/azurerm"
+      version  = "3.43.0"
+    }
+  }
+    cloud {
+      organization = "ramdharini"
+      workspaces {
+        name = "Terraformci1"
+      }
+    }
+  }
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "random_string" "uniquestring" {
+  length  = 20
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "811-6ef06349-provide-continuous-delivery-with-gith"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "stg${random_string.uniquestring.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
